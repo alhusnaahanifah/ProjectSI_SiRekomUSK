9720d46419d9b1ac4a9d0f46ac7e339db1dfbf5b
Nurul Uzratun Nashriyyah <130304310+NurulNashriyyah@users.noreply.github.com>
2025-05-08 10:16:26 +0700

    add library

--- sirekom/sirekom/settings.py
+++ sirekom/sirekom/settings.py
@@ -134,8 +134,13 @@ connect(
     host="mongodb://localhost:27017/sirekom"  # default MongoDB lokal
 )
 
+from django.contrib.messages import constants as messages
+
 MESSAGE_TAGS = {
+    messages.DEBUG: 'alert-secondary',
+    messages.INFO: 'alert-info',
     messages.SUCCESS: 'alert-success',
+    messages.WARNING: 'alert-warning',
     messages.ERROR: 'alert-danger',
 }
 
